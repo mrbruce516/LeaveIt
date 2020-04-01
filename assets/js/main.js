62d6eaca0e027f3c507f4d97c93087961c942cd5
爱摸鱼的坏船长 <46187947+mrbruce516@users.noreply.github.com>
2020-04-01 15:59:10 +0800

    更新

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ jQuery(function($) {
     _Blog.changeTitle = function() {
         var currentTitle = document.title;
         window.onblur = function() {
-            document.title = 'I miss you!（＞﹏＜）';
+            document.title = '君と出会えて 良かった (* ॑ᐜ ॑*)';
         }
         window.onfocus = function() {
             document.title = currentTitle;
